73676b2c32937ccd2c2faadc3eec1530d90460ae
veyseltosun <postalarim1923@gmail.com>
2022-12-21 05:53:31 +0300

    some more.

---
+++ src/auth/firebase.js
@@ -0,0 +1,17 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBW7-SmW1qSLkS2d4J3aOuSfUh73OIF2gU",
+//   authDomain: "react-movie-app2.firebaseapp.com",
+//   projectId: "react-movie-app2",
+//   storageBucket: "react-movie-app2.appspot.com",
+//   messagingSenderId: "1079319683000",
+//   appId: "1:1079319683000:web:5a07062ed7a308f3cb794d"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
