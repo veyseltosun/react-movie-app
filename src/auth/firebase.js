64d8973c69a85dd8075a9afb768baaf82011c3ac
veyseltosun <postalarim1923@gmail.com>
2022-12-22 11:41:50 +0300

    firebase.

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,19 +1,20 @@
 
 import { initializeApp } from "firebase/app";
-import { 
-    getAuth,
-    signOut,
-    onAuthStateChanged,
-   
-    
-    
-   
-  
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    
-   
-  } from 'firebase/auth';
+
+import {
+  getAuth,
+  signOut,
+  onAuthStateChanged,
+
+
+
+
+
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+
+
+} from 'firebase/auth';
 
 
 
@@ -31,86 +32,99 @@ const auth = getAuth(app)
 
 // Create new account using email/password
 
- export const createUser = async (email, password, displayName) => {
-  
-  
-    try {
-      await createUserWithEmailAndPassword(auth, email, password)
-      const currentUser = getAuth().currentUser;
-      await currentUser.updateProfile({ displayName });
-    }
-    catch(error) {
-      console.log(`There was an error: ${error}`)
+export const createUser = async (email, password, displayName) => {
 
-      
-      
-    }  
+
+  try {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const currentUser= userCredential.user;
+        // ...
+        
+        currentUser.updateProfile({ displayName });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+  }
+  catch (error) {
+    console.log(`There was an error: ${error}`)
+
+
 
   }
+
+}
 
 //   Monitor auth state
 const monitorAuthState = async () => {
-    onAuthStateChanged(auth, user => {
-      if (user) {
-        console.log(user)
-        
-      }
-      else {
-        console.log("You're not logged in.");
-     
-      }
-    })
-  }
-  
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+
+    }
+    else {
+      console.log("You're not logged in.");
+
+    }
+  })
+}
+
 
 
 // Login using email/password
 
 export const signIn = async (email, password) => {
-  
-  
-    // step 1: try doing this w/o error handling, and then add try/catch
+
+
+  // step 1: try doing this w/o error handling, and then add try/catch
+  await signInWithEmailAndPassword(auth, email, password)
+
+  // step 2: add error handling
+  try {
     await signInWithEmailAndPassword(auth, email, password)
-  
-    // step 2: add error handling
-    try {
-      await signInWithEmailAndPassword(auth, email, password)
-    }
-    catch(error) {
-      console.log(`There was an error: ${error}`)
-      
-    }
+    
   }
+  catch (error) {
+    console.log(`There was an error: ${error}`)
 
-  export const logOut = async () => {
-    await signOut(auth);
   }
+}
 
-  export const userObserver = async (setCurrentUser) => {
-    getAuth().onAuthStateChanged((user) => {
-      if (user) {
-        setCurrentUser(user);
-      } else {
-        // User is signed out
-        setCurrentUser(null);
-      }
-    });
-  };
-  
-  export const signUpProvider = () => {
-    var provider = new getAuth().GoogleAuthProvider();
-    provider.setCustomParameters({ prompt: "select_account" });
-    getAuth().signInWithPopup(provider);
-  };
-  
-  export const forgotPassword = (email) => {
-    getAuth().sendPasswordResetEmail(email);
-  
-    alert("Please check your mail box!");
-  };
- 
+export const logOut = async () => {
+  await signOut(auth);
+}
+
+export const userObserver = async (setCurrentUser) => {
+  getAuth().onAuthStateChanged((user) => {
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      // User is signed out
+      setCurrentUser(null);
+    }
+  });
+};
+
+export const signUpProvider = () => {
+  var provider = new getAuth().GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: "select_account" });
+  getAuth().signInWithPopup(provider);
+};
+
+export const forgotPassword = (email) => {
+  getAuth().sendPasswordResetEmail(email);
+
+  alert("Please check your mail box!");
+};
+
 //   connectAuthEmulator(auth, "http://localhost:9099");
 
-  monitorAuthState();
-  
-  export default app;
+monitorAuthState();
+
+export default app;
