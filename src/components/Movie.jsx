ddf40778c18fad324e4c64dd28934a1fb55dad5e
veyseltosun <postalarim1923@gmail.com>
2022-12-22 11:14:26 +0300

    small  change .

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -24,10 +24,11 @@ const Movie = ({title, poster_path, overview, vote_average}) => {
             {currentUser && <span className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</span>}
         </div>
 
-        <div className="movie-over">
-            <h2>Overview:</h2>
-            <p>{overview}</p>
-        </div>
+        {currentUser &&<div className="movie-over">
+        <h2>Overview:</h2>
+            <p>{overview}</p> 
+           
+        </div>}
     </div>
 )};
 
