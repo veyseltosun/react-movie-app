c17be7b0cf549eab1155a00386506354de32046b
veyseltosun <postalarim1923@gmail.com>
2022-12-22 10:28:09 +0300

    some more problem but working bug.

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,9 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth,
+    
+    
+   
+  
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+   
+  } from 'firebase/auth';
+
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBW7-SmW1qSLkS2d4J3aOuSfUh73OIF2gU",
   authDomain: "react-movie-app2.firebaseapp.com",
@@ -13,5 +22,95 @@ const firebaseConfig = {
   appId: "1:1079319683000:web:5a07062ed7a308f3cb794d"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+
+// Create new account using email/password
+
+ export const createUser = async (email, password, displayName) => {
+  
+  
+    try {
+      await createUserWithEmailAndPassword(auth, email, password)
+      const currentUser = getAuth().currentUser;
+      await currentUser.updateProfile({ displayName });
+    }
+    catch(error) {
+      console.log(`There was an error: ${error}`)
+
+      
+      
+    }  
+
+  }
+
+  // Monitor auth state
+// const monitorAuthState = async () => {
+//     onAuthStateChanged(auth, user => {
+//       if (user) {
+//         console.log(user)
+//         showApp()
+//         showLoginState(user)
+  
+//         hideLoginError()
+//         hideLinkError()
+//       }
+//       else {
+//         showLoginForm()
+//         lblAuthState.innerHTML = `You're not logged in.`
+//       }
+//     })
+//   }
+  
+
+
+// Login using email/password
+
+export const signIn = async (email, password) => {
+  
+  
+    // step 1: try doing this w/o error handling, and then add try/catch
+    await signInWithEmailAndPassword(auth, email, password)
+  
+    // step 2: add error handling
+    try {
+      await signInWithEmailAndPassword(auth, email, password)
+    }
+    catch(error) {
+      console.log(`There was an error: ${error}`)
+      
+    }
+  }
+
+  export const signOut = async () => {
+    await signOut(auth);
+  }
+
+  export const userObserver = async (setCurrentUser) => {
+    getAuth().onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        // User is signed out
+        setCurrentUser(null);
+      }
+    });
+  };
+  
+  export const signUpProvider = () => {
+    var provider = new getAuth().GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: "select_account" });
+    getAuth().signInWithPopup(provider);
+  };
+  
+  export const forgotPassword = (email) => {
+    getAuth().sendPasswordResetEmail(email);
+  
+    alert("Please check your mail box!");
+  };
+ 
+//   connectAuthEmulator(auth, "http://localhost:9099");
+
+//   monitorAuthState();
+  
+  export default app;
